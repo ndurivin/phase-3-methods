1866ead48a9d3299e26306dc9ee0096e1c256c5b
Vinn Nduri <ndurivo@gmail.com>
2022-08-16 17:51:10 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!" 
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!" 
+end
+
+def add (num1, num2)
+    return num1 + num2
+end
+sum = add(2,5)
+
+def halve(num)
+   if num.class != Integer
+    return nil
+   end
+    return num / 2
+end
+
+
+
